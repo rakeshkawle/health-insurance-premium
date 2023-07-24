8ad923b19243ce7f6f99a1e08d63d004a869df7a
rakeshkawle <kawlerakesh@gmail.com>
2023-07-24 23:26:36 +0530

    prediction pipelines completed

---
+++ src/pipelines/prediction_pipelines.py
@@ -0,0 +1,79 @@
+# Basic library
+import pandas as pd
+import numpy as np
+import sys
+import os
+
+# logger exception
+from src.logger import logging
+from src.exception import CustomException
+
+# models
+from src.utils  import load_object
+
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+
+    def predict(self,features):
+
+        try:
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            model_path=os.path.join('artifacts','model.pkl')
+
+            preprocessor = load_object(preprocessor_path)
+            model = load_object(model_path)
+
+            data_scaled=preprocessor.transform(features)
+
+            pred=model.predict(data_scaled)
+
+            return pred
+
+        except Exception as e:
+            logging.info('Exception occured in prediction')
+            raise CustomException(e,sys)
+        
+
+class CustomData:
+    def __init__(self,
+                 age:int,
+                 sex:str,
+                 bmi:float,
+                 children:int,
+                 smoker:str,
+                 region:str):
+        
+        # initialising the variables
+
+        self.age=age
+        self.sex=sex
+        self.bmi=bmi
+        self.children=children
+        self.smoker=smoker
+        self.region=region
+
+    def get_data_as_dataframe(self):
+        try:
+            custom_data_input_dict = {
+                'age':[self.age],
+                'sex':[self.sex],
+                'bmi':[self.bmi],
+                'children':[self.children],
+                'smoker':[self.smoker],
+                'region':[self.region]
+            }
+
+            df=pd.DataFrame(custom_data_input_dict)
+            logging.info('Dataframe Gathered')
+            return df
+        
+        except Exception as e:
+            logging.info('Exception Occured in prediction pipeline')
+            raise CustomException(e,sys)
+
+
+
+
